e375ea28fab5d1e631bb92965995bafa247c573f
jflopes21 <joaofrancisco21@yahoo.com.br>
2024-03-04 17:50:18 -0300

    Add: Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,13 @@
 import { Container } from "./styles";
 
+import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>João Francisco</span>
-
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
+      <Header/>
       <Button title="Voltar" />
     </Container>
   );
