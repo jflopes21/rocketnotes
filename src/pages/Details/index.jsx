d2b79808d2a54f9fa5caf018ffb1629c326c82a5
Joao Francisco <joaofrancisco21@yahoo.com.br>
2024-03-05 22:21:07 -0300

    Add: ButtonText, Section, Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,32 @@
 import { Container } from "./styles";
-
+import { Section } from "../../components/Section";
+import { Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
-
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
-      <Header/>
+      <Header />
+
+      <ButtonText title="Excluir nota" />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
       <Button title="Voltar" />
     </Container>
   );
