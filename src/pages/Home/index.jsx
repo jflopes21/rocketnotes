5d6a7b4807f19823fe569d4360f6fe6ad5bd01d8
Joao Francisco <joaofrancisco21@yahoo.com.br>
2024-03-12 21:51:57 -0300

    Finished: Home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,9 @@
-import {FiPlus} from 'react-icons/fi'
+import {FiPlus, FiSearch} from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { Note } from "../../components/Note";
+import { Section } from "../../components/Section";
 import { ButtonText } from "../../components/ButtonText";
 
 export function Home() {
@@ -19,11 +22,20 @@ export function Home() {
       </Menu>
 
       <Search>
-
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
       </Search>
 
       <Content>
-
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React', 
+            tags: [
+              {id: '1', name:'react'},
+              {id: '2', name:'rocketseat'},
+            ]
+        
+          }}/>
+        </Section>
       </Content>
 
       <NewNote>
