195b7e08bd19f2b8256e19d83fac4fe4355ff0ca
jflopes21 <joaofrancisco21@yahoo.com.br>
2024-03-15 11:12:18 -0300

    Add: Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home } from "./pages/Home/";
-import { SignUp } from "./pages/SignUp/";
-import { SignIn } from "./pages/SignIn/";
-
-import { Details } from "./pages/Details/";
-import { New } from "./pages/New/";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
-
 import theme from "./styles/theme";
+
+import { Routes } from "./routes"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 );
